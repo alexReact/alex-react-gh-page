c3b97ee2706be1b8fc87022cfb515fdea8444cc3
AlexReact <alexreact88@gmail.com>
2020-03-17 08:10:08 +0200

    Updates

---
+++ precache-manifest.1022caf8fbc32a790524ab1a12f5eb69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e3d87bbcbc739cf64b460500b57664",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d2b849591a07b2fca9fa",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81d9229396f3704119be",
+    "url": "/react-app/static/js/2.59532696.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app/static/js/2.59532696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b849591a07b2fca9fa",
+    "url": "/react-app/static/js/main.9247e977.chunk.js"
+  },
+  {
+    "revision": "87bc26fc6a7728394651",
+    "url": "/react-app/static/js/runtime-main.6dcf0eda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);

51cdb41f5c3fd808299ba32eb15fa04897506a8b
AlexReact <alexreact88@gmail.com>
2020-04-04 16:17:17 +0300

    Updates

---
+++ precache-manifest.6bcc4543269b743d69a3a4c09fcc113c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445b205105747f8c15f1754b77463695",
+    "url": "/alex-react-gh-page/index.html"
+  },
+  {
+    "revision": "008e478eb8097e3bc8a7",
+    "url": "/alex-react-gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81d9229396f3704119be",
+    "url": "/alex-react-gh-page/static/js/2.59532696.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/alex-react-gh-page/static/js/2.59532696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008e478eb8097e3bc8a7",
+    "url": "/alex-react-gh-page/static/js/main.c0d79e1d.chunk.js"
+  },
+  {
+    "revision": "fde0b4944ab9886a6cc6",
+    "url": "/alex-react-gh-page/static/js/runtime-main.175a0e8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alex-react-gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.1022caf8fbc32a790524ab1a12f5eb69.js"
+  "/alex-react-gh-page/precache-manifest.6bcc4543269b743d69a3a4c09fcc113c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alex-react-gh-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

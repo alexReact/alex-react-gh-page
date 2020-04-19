586c6b1fe46faa1f3f7e2908a55273330f4aee09
AlexReact <alexreact88@gmail.com>
2020-04-19 10:48:53 +0300

    added active menu class for side menu

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -3,44 +3,43 @@ import { mainMenu } from "./model";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
-interface IHeaderProps {
-  [key: string]: any;
-}
-
-export const Header: React.FC<IHeaderProps> = () => {
-  const [activeMenu, setActiveMenu] = useState<string>(
+export const Header = () => {
+  const [activeMenu, setActiveMenu] = useState<Array<string> | string>(
     window.location.hash.slice(1)
   );
-  const [r, setR] = useState<boolean>(false);
+  const [reload, setReload] = useState<boolean>(false);
 
   useEffect(() => {
-    setActiveMenu(window.location.hash.slice(1));
-  }, [r]);
+    setActiveMenu(window.location.hash.split("/"));
+  }, [reload]);
 
   console.log(activeMenu);
   return (
     <nav className="nav flex-column">
       {mainMenu.map(({ link, title, subMenu }) => {
-        const actMenu = activeMenu === link ? "text-dark" : "";
+        const actMenu = `/${activeMenu[1]}` === link ? "text-dark" : "";
 
         return (
           <Link
             to={link}
             key={link}
             className={`nav-link ${actMenu}`}
-            onClick={() => setR(!r)}
+            onClick={() => setReload(!reload)}
           >
             {title}
             {Array.isArray(subMenu) &&
+              `/${activeMenu[1]}` === link &&
               subMenu?.map(({ link, title }) => {
-                const activeSubMenu = activeMenu === link ? "text-success" : "";
+                const activeSubMenu =
+                  `/${activeMenu[1]}/${activeMenu[2]}` === link
+                    ? "text-success"
+                    : "";
 
                 return (
                   <Link
                     to={link}
                     key={link}
                     className={`nav-link ${activeSubMenu}`}
-                    // onClick={() => onHandlerActiveMenu(link)}
                   >
                     {title}
                   </Link>
